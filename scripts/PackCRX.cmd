13f361a490fe93bbccf57edab128f3165428ea6d
Isaac Shoebottom <ir.shoebottom@gmail.com>
2022-06-29 05:11:10 -0300

    Add license and scripts

---
+++ scripts/PackCRX.cmd
@@ -0,0 +1,3 @@
+:: A little bit broken, places files in folder above git folder?
+
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --pack-extension="C:\GitHub\ChromeExtensions\SearchSwitch"
