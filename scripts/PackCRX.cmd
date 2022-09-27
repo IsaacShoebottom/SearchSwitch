03de07c043b026d1b102e299c3c847b440a693f0
Isaac Shoebottom <ir.shoebottom@gmail.com>
2022-09-27 18:19:10 -0300

    Move files to start maifest v2 dev

--- scripts/PackCRX.cmd
+++
@@ -1,3 +0,0 @@
-:: A little bit broken, places files in folder above git folder?
-
-"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --pack-extension="C:\GitHub\ChromeExtensions\SearchSwitch"
